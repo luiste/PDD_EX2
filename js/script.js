f657901cc1d2445ed3ddf800e52b8cd58449a685
Luís Santos <luis@luissantos.org>
2017-06-11 10:54:48 +0100

    002

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     
     
     
-//CHANGE_BG_HOME #######################    
+//CHANGE_BG_HOME #################################################################
+    
   $(window).bind('scroll', function() {
     var distance = 180;
     if ($(window).scrollTop() > distance) {
@@ -15,17 +16,50 @@ $(document).ready(function(){
   });
     
     
+//CHANGE_COLOR HEADER ##############################################################
+    
+    $(window).bind('scroll', function() {
+        var distance = 180;
+      
+        if ($(window).scrollTop() > distance) {
+            $('header').addClass('colorHeader');
+        }
+          else {
+      $('header').removeClass('colorHeader');
+    }
+    });
     
     
     
-    
-//WORK_THUMBNAILS #######################
+//WORK_THUMBNAILS #################################################################
     
     $(".main").mouseover(function(){
+        var posTop = Math.random(0,1);
+        
         $(".image").show();
+        $(".image").css('margin-top', posTop);
+        $(".image").css('margin-left', posTop);
     });
     
     $(".main").mouseout(function(){
         $(".image").hide();
     });   
 });
+
+
+//MODAL_WORK ######################################################################
+
+ $('.main').on('click', function() {
+    console.log('CLICK');
+
+              
+        $('.modal').addClass('modal-isOpen');
+     
+    });
+
+    $('.modal-close').on('click', function() {
+        $('.modal').removeClass('modal-isOpen')
+    });
+    
+
+
