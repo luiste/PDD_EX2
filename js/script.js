0b21de42961b4c25eb740fbb2434437c38734544
Luís Santos <luis@luissantos.org>
2017-06-15 16:45:02 +0100

    006

--- js/script.js
+++ js/script.js
@@ -33,8 +33,8 @@ $(document).ready(function(){
     
 //WORK_THUMBNAILS #################################################################
     
-    $(".main").mouseover(function(){
-        var posTop = Math.random() * 100;
+$(".main").mouseover(function(){
+        var posTop = Math.random() * 400;
         console.log(posTop)
         
         $(".image").show();
@@ -48,20 +48,57 @@ $(document).ready(function(){
 });
 
 
+
+
+
 //MODAL_WORK ######################################################################
 
- $('.thumb').on('click', function() {
+$('.thumb').on('click', function() {
+    var marginLeft = -0.3;
     console.log('CLICK');
-
-              
+ 
         $('.modal').addClass('modal-isOpen');
-//     add class body
-     
-    });
+        $('body').css({
+            'overflow-y': 'hidden'
+        });
+        $("header").css('margin-left', marginLeft + 'em');
+});
 
     $('.modal-close').on('click', function() {
-        $('.modal').removeClass('modal-isOpen')
+        $('.modal').removeClass('modal-isOpen');
+        $('body').css({
+            'overflow-y' : 'auto'
+        });
     });
+
+
+
+
+
+
+//MODAL_WORK ######################################################################
+
+//$('.thumb').on('click', function() {
+//    console.log('CLICK');
+// 
+//        $('.modal').addClass('modal-isOpen');
+//        $('body').css({
+//            'overflow-y': 'hidden'
+//        });
+//    
+//});
+//
+//    $('.modal-close').on('click', function() {
+//        $('.modal').removeClass('modal-isOpen');
+//        $('body').css({
+//            'overflow-y' : 'auto'
+//        });
+//    });
+
+
+
+
+
 
 
 
